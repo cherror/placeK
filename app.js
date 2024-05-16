5186cf95ee8513518d5d54b24713e047a9da2041
judyzero <judyzero@github.com>
2024-05-16 17:37:47 +0900

    MongoDB 연결

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
 const app = express();
+dotenv.config();
 
 app.set('port', process.env.PORT || 3000);
 
@@ -10,3 +14,13 @@ app.get('/', (req, res) => {
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중')
 })
+
+try {
+    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+    mongoose.connection.once("open", () => {
+      console.log("MongoDB is Connected");
+    });
+  } catch (error) {
+    console.error("mongoDB error");
+    console.log(error);
+  }
